a021fccac96cf20a2298cec1df530bcb50a0044c
Ruben Quizon <quizon@fullsail.edu>
2013-08-28 11:30:36 -0500

    added filler data

--- js/json.js
+++ js/json.js
@@ -2,7 +2,23 @@ var fillerData = {
 	"item1": {
 		"itype": ["Item Type", "Weapon"],
 		"iname": ["Item Name", "Mace"],
-		""
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "No"],
+		"inotes": ["Notes", "...Windu"]
+	},
+	"item2": {
+		"itype": ["Item Type", "Food"],
+		"iname": ["Item Name", "Slim Jim"],
+		"inumber": ["How Many", "5"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "There should always be one in your hands"]
+	},
+	"item3": {
+		"itype": ["Item Type", "Vehicle"],
+		"iname": ["Item Name", "Mystery Machine"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "No"],
+		"inotes": ["Notes", "Because Yes."]
 	}
 	
 }
